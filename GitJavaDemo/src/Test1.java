5fbe8ae81ab074b2f64fc314e38393ab17c3d752
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-01-03 16:21:41 -0600

    changed check conditions in Test1 class

--- GitJavaDemo/src/Test1.java
+++ GitJavaDemo/src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		String hungry = "yes";
 
-		if (hungry == "yes") {
+		if (hungry == "yes" || hungry == "Y" || hungry == "y") {
 			System.out.println("Go and eat!");
 		} else {
 			System.out.println("Go and work!");
